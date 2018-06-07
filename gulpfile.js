fe6d57d6b4daa990bf69f179d8bd2c04ee03a6d2
Ilyas Kabirov <ilyas.kabirov@gmail.com>
2018-06-07 18:21:57 +0300

    check task was added

--- gulpfile.js
+++ gulpfile.js
@@ -20,3 +20,20 @@ gulp.task('default', () => {
 	.src() // needed to force the plugin to run without gulp.src
 	.pipe(gulp.dest('./dist/js'));
 });
+
+gulp.task('check', () => {
+	return closureCompiler({
+		js: path.join(__dirname, './src/**.js'),
+		entry_point: path.join(__dirname, './src/index.js'),
+		compilation_level: 'ADVANCED',
+		warning_level: 'VERBOSE',
+		checks_only: 1,
+		language_in: 'ECMASCRIPT_NEXT',
+		language_out: 'ECMASCRIPT5_STRICT',
+		dependency_mode: 'STRICT',
+		output_wrapper: '(function(){\n%output%\n}).call(this)',
+		js_output_file: 'output.min.js'
+	})
+		.src() // needed to force the plugin to run without gulp.src
+		.pipe(gulp.dest('./dist/js'));
+});
